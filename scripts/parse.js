0bcb9a3574c96c03da9ec803840978d3b7aa654f
zoheiry <ali.elzoheiry@gmail.com>
2021-08-21 23:52:31 +0200

    Update to use react-data-table with functioning pagination and sorting

--- scripts/parse.js
+++ scripts/parse.js
@@ -3,8 +3,8 @@ const fs = require('fs');
 
 const data = fs.readFileSync('scripts/input/data.csv').toString().split('\n');
 
-const INCOMING = 'Bij';
-const OUTGOING = 'Af';
+const INCOMING = 'Credit';
+const OUTGOING = 'Debit';
 
 const CODES = {
 	BA: 'terminal',
@@ -28,7 +28,7 @@ const clean = (s) => s.replace(/"/g, '');
 const total = (transactions) => transactions.reduce((sum, t) => sum + t.amount, 0);
 
 const transactions = data.map(row => {
-	const transaction = row.split('","').map(clean);
+	const transaction = row.split('";"').map(clean);
 	const dateString = transaction[0];
 	const date = `${dateString.substr(0, 4)}-${dateString.substr(4, 2)}-${dateString.substr(6, 2)}`;
 	return {
@@ -38,31 +38,11 @@ const transactions = data.map(row => {
 		toAccount: transaction[3],
 		code: transaction[4],
 		type: CODES[transaction[4]],
-		outgoing: transaction[5] === OUTGOING,
-		incoming: transaction[5] === INCOMING,
-		amount: transaction[6] ? parseFloat(transaction[6].replace(',', '.')) : 0,
+		outgoing: transaction[5] === OUTGOING ? 'true' : '',
+		incoming: transaction[5] === INCOMING ? 'true' : '',
+		amountInCents: transaction[6] ? parseFloat(transaction[6].replace(',', '.')).toFixed(2) * 100 : 0,
 		notes: transaction[8],
 	}
 });
 
 fs.writeFileSync('src/transactions.json', JSON.stringify(transactions, null, 2));
-
-const outgoingTransactions = transactions.filter(t => t.outgoing && !t.name.includes('Elzoheiry'));
-const incomingTransactions = transactions.filter(t => t.incoming);
-const totalOut = total(outgoingTransactions);
-const totalIn = total(incomingTransactions);
-// console.log(totalOut);
-// console.log(totalIn);
-
-const fashiontradeTransactions = incomingTransactions.filter(t => t.name.includes('Fashiontrade'));
-console.log(fashiontradeTransactions.length);
-console.log(total(fashiontradeTransactions));
-// console.log(filteredTransactions);
-
-
-// const input = process.stdin;
-// input.setEncoding('utf-8');
-// input.on('data', function (data) {
-// 	console.log('User Input Data : ' + data);
-// 	process.exit();
-// });
--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Table } from '@fashiontrade/wardrobe';
+import DataTable, { createTheme } from 'react-data-table-component';
 import transactions from './transactions.json';
 import styled from 'styled-components';
 
@@ -7,36 +7,75 @@ const Wrapper = styled('div')`
   padding: 40px;
   margin: 0 auto;
 `;
-                
-const LAYOUT = [0.7, 1, 1, 1, 0.5, 1, 0.5, 0.5, 0.5, 1];
+
+const columns = [
+  {
+    name: 'Date',
+    selector: 'date',
+    sortable: true,
+    width: '150px',
+  },
+  {
+    name: 'Name',
+    selector: 'name',
+    sortable: true,
+    width: '300px',
+  },
+  {
+    name: 'To account',
+    selector: 'toAccount',
+    sortable: true,
+    width: '200px',
+  },
+  {
+    name: 'Code',
+    selector: 'code',
+    sortable: true,
+    width: '100px',
+  },
+  {
+    name: 'Type',
+    selector: 'type',
+    sortable: true,
+    width: '150px',
+  },
+  {
+    name: 'Outgoing',
+    selector: 'outgoing',
+    sortable: true,
+    width: '100px',
+  },
+  {
+    name: 'Incoming',
+    selector: 'incoming',
+    sortable: true,
+    width: '100px',
+  },
+  {
+    name: 'Amount',
+    selector: (row) => parseFloat(row.amountInCents / 100).toFixed(2),
+    sortable: true,
+    sortFunction: (rowA, rowB) => rowA.amountInCents > rowB.amountInCents ? 1 : -1,
+    width: '100px',
+  },
+  {
+    name: 'Notes',
+    selector: 'notes',
+    sortable: true,
+    width: '400px',
+    wrap: true,
+  },
+]
 
 function App() {
   return (
     <Wrapper>
-      <Table layout={LAYOUT}>
-        <Table.Header>
-          <Table.Cell>Date</Table.Cell>
-          <Table.Cell>Timestamp</Table.Cell>
-          <Table.Cell>Name</Table.Cell>
-          <Table.Cell>To account</Table.Cell>
-          <Table.Cell>Code</Table.Cell>
-          <Table.Cell>Type</Table.Cell>
-          <Table.Cell>Outgoing</Table.Cell>
-          <Table.Cell>Incoming</Table.Cell>
-          <Table.Cell>Amount</Table.Cell>
-          <Table.Cell>Notes</Table.Cell>
-        </Table.Header>
-        {transactions.map((t, i) =>
-          <Table.Row key={`transaction-${i}`}>
-            {Object.keys(t).map((key, index) => {
-              if (key === 'incoming' || key === 'outgoing') {
-                return <Table.Cell>{(t[key] ? 'true' : '')}</Table.Cell>  
-              }
-              return <Table.Cell>{t[key]}</Table.Cell>
-            })}
-          </Table.Row>
-        )}
-      </Table>
+      <DataTable
+        columns={columns}
+        data={transactions}
+        striped
+        pagination
+      />
     </Wrapper>
   );
 }
